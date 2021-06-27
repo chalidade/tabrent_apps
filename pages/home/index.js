d39892ac792bb1062521511f3e9034735cfcce28
Ekky Sukmawati <ekkys99@gmail.com>
2021-06-27 14:31:33 +0700

    Update Home - Button
    - Button boostrap
    - Material Design Icon

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,7 +5,8 @@ import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { DirectionsBike, Motorcycle, DirectionsBus } from '@material-ui/icons';
-import Icon from '@material-ui/core/Icon';
+import { FaMotorcycle, FaBus, FaBicycle } from 'react-icons/fa';
+import { MdDirectionsBike, MdMotorcycle, MdDirectionsBus } from "react-icons/md";
 
 
 export default function Index() {
@@ -13,7 +14,7 @@ export default function Index() {
     <div>
       <TopNav back="true" text="Home"/>
       <div className="main">
-          <div class="row col-md-12 mt-5" radius="10px">
+          <div class="row col-md-12 mt-3" radius="10px">
           <Carousel>
             <Carousel.Item interval={2000}>
               <ImageBanner image="/home/banner_1.jpg"  width="306px" height="150px" repeat="no-repeat"  radius="10px" />
@@ -35,17 +36,40 @@ export default function Index() {
           </Carousel>
           </div>
 
-          <div class="row col-md-12 mt-5">
+          <div class="row col-md-12 mt-3">
           <center>
-            <ButtonGroup aria-label="Basic example">
-              <Button variant="primary" style={{backgroundColor: "#2F2F8D", color:"#ffffff"}}>Bycycle</Button>
-              <Button variant="primary" style={{backgroundColor: "#2F2F8D", color:"#ffffff"}}>Motor Cycle</Button>
-              <Button variant="primary" style={{backgroundColor: "#2F2F8D", color:"#ffffff"}}>Bus</Button>
-            </ButtonGroup>
+                <Button variant="primary" style={{
+                  backgroundColor: "#2F2F8D",
+                  color:"#ffffff",
+                  height:"49px",
+                  width:"109px",
+                  left:"0px",
+                  top:"-1px",
+                  borderRadius: "15px"
+                }}>
+                <MdDirectionsBike /> Bycycle </Button>
 
-            <Icon>
-            star
-            </Icon>
+                <Button variant="primary" style={{
+                  backgroundColor: "#2F2F8D",
+                  color:"#ffffff",
+                  height:"49px",
+                  width:"109px",
+                  left:"0px",
+                  top:"-1px",
+                  borderRadius: "15px"
+                }}>
+              <MdMotorcycle /> Motor Cycle </Button>
+
+                <Button variant="primary" style={{
+                  backgroundColor: "#2F2F8D",
+                  color:"#ffffff",
+                  height:"49px",
+                  width:"109px",
+                  left:"0px",
+                  top:"-1px",
+                  borderRadius: "15px"
+                }}>
+                <MdDirectionsBus /> Bus </Button>
             </center>
           </div>
 
