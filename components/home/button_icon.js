8a46bcb39aad934caacafa4cc7f6ed60e9b03480
Ekky Sukmawati <ekkys99@gmail.com>
2021-07-09 22:48:11 +0700

    Update Home - Scroll Overlay
    - Button Icon Dinamis
    - OverlayX scroll button
    - Bottom Nav fixing

--- components/home/button_icon.js
+++ components/home/button_icon.js
@@ -1,39 +1,50 @@
-import { Container, Typography } from "@material-ui/core";
-import { Card,Button, Carousel} from 'react-bootstrap';
-import { MdDirectionsBike, MdMotorcycle, MdDirectionsBus, MdDirectionsCar } from "react-icons/md";
+import {
+  Container,
+  Typography
+} from "@material-ui/core";
+import {
+  Card,
+  Button,
+  Carousel
+} from 'react-bootstrap';
+import {
+  MdDirectionsBike,
+  MdMotorcycle,
+  MdDirectionsBus,
+  MdDirectionsCar
+} from "react-icons/md";
 
-const styles= {
+const styles = {
   btnItem: {
-    backgroundColor:"#2F2F8D",
-    borderColor:"#2F2F8D",
-    color:"#ffffff",
-    width:"90px",
-    height:"50px",
-    left:"0px",
-    top:"-1px",
+    backgroundColor: "#2F2F8D",
+    borderColor: "#2F2F8D",
+    color: "#ffffff",
+    width: "90px",
+    height: "50px",
+    left: "0px",
+    top: "-1px",
     borderRadius: "15px",
-    margin : "3px 0px 0px 0px",
-    justifyContent : "center"
+    marginRight: "5px",
+    justifyContent: "center"
   }
 };
 
 
-export default function ButtonIcon(){
-  return(
-        <div className="row" style={{ height: "auto", align:"justify" }}>
-          <div className="col">
-          <Button size="sm" style={styles.btnItem}>
-          <MdDirectionsBike/> Bycycle</Button>
-          </div>
-          <div className="col">
-          <Button size="sm" style={styles.btnItem}>
-          <MdMotorcycle/> Motor Cycle</Button>
-          </div>
-          <div className="col">
-          <Button size="sm" style={styles.btnItem}>
-          <MdDirectionsCar/> Car</Button>
-          </div>
-        </div>
-
-  );
-}
+export default function ButtonIcon({
+  type,
+  value
+}) {
+  return (
+    <
+    td
+       > {
+        type == 'Bycycle' ? ( < Button size = "sm"
+          style = {
+            styles.btnItem
+          } >
+          <
+          MdDirectionsBike / > Bycycle < /Button>): type == 'MotorCycle' ? (<Button size="sm" style={styles.btnItem}> <
+          MdMotorcycle / > Motor Cycle < /Button>): ''} <
+          /td>
+        );
+      }
